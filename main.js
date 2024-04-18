4a00c0a7b9ba8f19aeca3cba8cbe524fc26dc231
lobosilencioso <105953274+lobosilencioso@users.noreply.github.com>
2024-04-18 20:33:33 -0300

    gf

--- main.js
+++ main.js
@@ -26,6 +26,6 @@ function calculaTempo(tempoObjetivo1) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    return dias; + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
+    return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
 }
 
