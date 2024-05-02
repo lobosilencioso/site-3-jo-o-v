69c6e298adabb9f16d956adc34aef3c332d32ba2
lobosilencioso <105953274+lobosilencioso@users.noreply.github.com>
2024-05-02 20:17:24 -0300

    ttt

--- main.js
+++ main.js
@@ -20,10 +20,11 @@ const tempoObjetivo2 = new Date('2025-10-05T00:00:00');
 const tempoObjetivo3 = new Date('2025-10-05T00:00:00');
 const tempoObjetivo4 = new Date('2025-10-05T00:00:00');
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-contadores[2].textContent = calculaTempo(tempoObjetivo3);
-contadores[3].textContent = calculaTempo(tempoObjetivo4);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4] ;
+
+for (let i = 0; i < contadores.length; i++) {
+    contadores [i].textContent = calculaTempo (tempos[i]);
+}
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();
