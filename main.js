9a8a7348f10b3feaf6b4da63d15e1f0c08f10eb8
lobosilencioso <105953274+lobosilencioso@users.noreply.github.com>
2024-05-16 19:59:30 -0300

    ygh

--- main.js
+++ main.js
@@ -25,7 +25,10 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4] 
 
 function atualizaCronometro(){
 for (let i = 0; i < contadores.length; i++) {
-    contadores [i].textContent = calculaTempo (tempos[i]);
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
 }
 }
 
@@ -34,7 +37,7 @@ atualizaCronometro ();
 setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();
@@ -49,9 +52,10 @@ function calculaTempo(tempoObjetivo1) {
     horas %= 24;
 
     if (tempoFinal > 0) {
-    return dias + 'dias ' + horas + 'horas ' + minutos + 'minutos ' + segundos + 'segundos ' ;
+    return [dias, horas, minutos, segundos] ;
 }
 else {
-    return 'Tempo Esgotado';
+    return [0,0,0,0];
 }
 }
+
